8796ebe4c12deb9d20afd831aa75f7aeae4146dc
indigoxela <user@olga.indigofloat.at>
2023-09-28 17:33:38 +0200

    Play with icon registry

--- js/plugins/imce/plugin.js
+++ js/plugins/imce/plugin.js
@@ -171,18 +171,6 @@ tinymce.PluginManager.add('imce', function(editor, url) {
     scope: 'node',
     position: 'node'
   });
-
-  // All the custom icons used by this plugin.
-  let lowVision = '<svg width="20" height="16" version="1.1" viewBox="0 0 20 16"><path d="m2.6923 0-1.0694 1.0509 2.7376 2.6902a10.001 4.9218 0 0 0-4.3604 4.0624 10.001 4.9218 0 0 0 9.999 4.9217 10.001 4.9218 0 0 0 3.2339-0.26322l3.6002 3.5379 1.0694-1.0509-2.9266-2.876a10.001 4.9218 0 0 0 5.0241-4.2695 10.001 4.9218 0 0 0-10.001-4.9217 10.001 4.9218 0 0 0-0.092568 0 10.001 4.9218 0 0 0-3.87 0.4045zm10.395 4.2908a8.8211 3.7511 0 0 1 5.7331 3.5128 8.8211 3.7511 0 0 1-4.6677 3.3095 5.0279 4.8391 31.428 0 0-0.57312-6.3965 5.0279 4.8391 31.428 0 0-0.068932-0.067739 5.0279 4.8391 31.428 0 0-0.42344-0.35805zm-7.8149 0.34644 6.9188 6.801a8.8211 3.7511 0 0 1-2.192 0.11612 8.8211 3.7511 0 0 1-8.8193-3.7508 8.8211 3.7511 0 0 1 4.0926-3.1663zm4.7642 0.085157a3.4546 3.3949 0 0 1 2.4618 0.92899 3.4546 3.3949 0 0 1 0.06499 0.063868 3.4546 3.3949 0 0 1 0.37814 4.3566l-1.0615-1.0412a1.9938 1.9593 0 0 0-0.35057-2.2993 1.9938 1.9593 0 0 0-0.02167-0.023224 1.9938 1.9593 0 0 0-2.32-0.32321l-1.0596-1.0412a3.4546 3.3949 0 0 1 1.9084-0.62127z"/></svg>';
-  editor.ui.registry.addIcon('lowvision', lowVision);
-  let floatNone = '<svg width="20" height="17" version="1.1" viewBox="0 0 20 17"><g fill="none" stroke="#000" stroke-width="2"><path d="m1 1h9v10h-9z" stroke-linecap="square"/><path d="m13 11h7"/><path d="m0 16h20"/></g></svg>';
-  editor.ui.registry.addIcon('floatnone', floatNone);
-  let floatLeft = '<svg width="20" height="17" version="1.1" viewBox="0 0 20 17"><rect x="1" y="1" width="9" height="10" style="fill:none;stroke-linecap:round;stroke-width:2;stroke:#000"/><path d="m13 11h7" style="fill:none;stroke-width:2;stroke:#000"/><path d="m0 16h20" style="fill:none;stroke-width:2;stroke:#000"/><path d="m13 1h7" style="fill:none;stroke-width:2;stroke:#000"/><path d="m13 6h7" style="fill:none;stroke-width:2;stroke:#000"/></svg>';
-  editor.ui.registry.addIcon('floatleft', floatLeft);
-  let alignCenter = '<svg width="20" height="17" version="1.1" viewBox="0 0 20 17"><g fill="none" stroke="#000" stroke-width="2"><rect x="5" y="5" width="10" height="7" stroke-linecap="round"/><path d="m0 16h20"/><path d="m0 1h20"/></g></svg>';
-  editor.ui.registry.addIcon('aligncenter', alignCenter);
-  let floatRight = '<svg width="20" height="17" version="1.1" viewBox="0 0 20 17"><g fill="none" stroke="#000" stroke-width="2"><rect transform="scale(-1,1)" x="-19" y="1" width="9" height="10" stroke-linecap="round"/><g><path d="m7 11h-7"/><path d="m0 16h20"/><path d="m7 1h-7"/><path d="m7 6h-7"/></g></g></svg>';
-  editor.ui.registry.addIcon('floatright', floatRight);
 });
 
 const imceTools = {}
